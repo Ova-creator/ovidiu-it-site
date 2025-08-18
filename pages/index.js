90a7a596749077431e7a639237f8d7c4f9ca5a5b
Ova-creator <ovidiu26.ova@gmail.com>
2025-08-18 17:23:11 +0100

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-cover bg-center text-white"
+         style={{ backgroundImage: "url('/bg.jpeg')" }}>
+      <h1 className="text-5xl font-bold">Ovidiu Website</h1>
+    </div>
+  );
+}
