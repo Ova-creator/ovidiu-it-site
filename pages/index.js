1fab3d11aff51c631ca7434a325263d1c3f9d3b7
Ova-creator <ovidiu26.ova@gmail.com>
2025-08-18 18:30:37 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
-
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen bg-cover bg-center text-white"
-         style={{ backgroundImage: "url('/bg.jpeg')" }}>
-      <h1 className="text-5xl font-bold">Ovidiu Website</h1>
+    <div className="flex items-center justify-center h-screen bg-blue-900 text-white">
+      <h1 className="text-5xl font-bold">✅ Ovidiu Website - Online!</h1>
     </div>
   );
 }
