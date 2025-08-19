eac10e4b444c3de365b21a0578b1304491387db5
Ova-creator <ovidiu26.ova@gmail.com>
2025-08-19 11:21:06 +0100

    Add files via upload

---
+++ pages/automation/index.js
@@ -0,0 +1,3 @@
+export default function Automation() {
+  return <div className="p-8 text-white">Automation Services Page - Smart automations that save time</div>;
+}
---
+++ pages/seo/index.js
@@ -0,0 +1,3 @@
+export default function SEO() {
+  return <div className="p-8 text-white">SEO Services Page - Optimized for Search Engines</div>;
+}
