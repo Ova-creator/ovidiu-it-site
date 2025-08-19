f1866c3602b00e9ad2284da073dcf007827287f3
Ova-creator <ovidiu26.ova@gmail.com>
2025-08-19 10:25:59 +0100

    Add files via upload

---
+++ pages/contact.js
@@ -0,0 +1,9 @@
+export default function Contact() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen text-white bg-gray-900">
+      <h1 className="text-4xl font-bold mb-4">Contact</h1>
+      <p>Email: contact@ovidiu.it</p>
+      <p>LinkedIn: linkedin.com/in/ovidiu</p>
+    </div>
+  );
+}
