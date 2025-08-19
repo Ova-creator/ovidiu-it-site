0889ec340e013c5ac76c5a6504101c212fd9fbb5
Ova-creator <ovidiu26.ova@gmail.com>
2025-08-19 11:15:13 +0100

    Delete pages directory

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-cover bg-center text-white"
-         style={{ backgroundImage: "url('/bg.jpeg')" }}>
-      <h1 className="text-5xl font-bold mb-4">Ovidiu Website</h1>
-      <p className="text-xl">Helping tech freelancers and digital nomads build powerful personal brands using AI.</p>
-    </div>
-  );
-}
